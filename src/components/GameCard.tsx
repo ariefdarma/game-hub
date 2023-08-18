a703ba290d7af28a3657c7dad8f81cccd0a01d90
crypt <arief.darma@proton.me>
2023-08-18 23:19:36 +0700

    show loading skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={30} overflow="hidden">
+    <Card width="300px" borderRadius={30} overflow="hidden">
       <Image src={getCroppefImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
